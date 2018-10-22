3413296a5f42d9755c43e8a97ea027df808ebb52
1asalinas <42749574+1asalinas@users.noreply.github.com>
2018-10-22 13:37:47 -0500

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,45 @@
+function positiveIntSum(limit){
+    let sum = 0;
+    for(let k=1; k<=limit; k++){
+        sum = sum + k;
+    }
+    return sum;
+}
+
+
+function addSum(start, diff, max){
+    //setting up varaibles
+    let sum = 0;
+    let currentNumber = start;
+
+    while(currentNumber <= max){
+        //adding to the sum.
+        sum = sum + currentNumber;
+        //moving to the next number in the list.
+        currentNumber = currentNumber + diff;
+    }
+
+    return sum;
+}
+
+function multSum(start, factor, max){
+    //setting up varaibles
+    let sum = 0;
+    let currentNumber = start;
+
+    while(currentNumber <= max){
+        //adding to the sum.
+        sum = sum + currentNumber;
+        //moving to the next number in the list.
+        currentNumber = currentNumber * factor;
+    }
+
+    return sum;
+}
+console.log(positiveIntSum(1500));
+console.log(positiveIntSum(2000000));
+console.log(addSum(-80, 60, 2860));
+console.log(addSum(10, 5, 100));
+console.log(multSum(4, 3, 50000));
+console.log(multSum(7, 2, 50000));
+console.log(multSum(2, -3, 1000000));
